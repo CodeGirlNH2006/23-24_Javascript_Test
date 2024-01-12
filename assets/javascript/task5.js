307df0550ef593bde780145aa437c7b8eb17d182
Noelle H <noelle.hernandez@vtsd.com>
2024-01-12 13:23:04 -0500

    1/12 - finished project

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,28 +1,28 @@
 /* Put code for the task below */
-const fullName = document.getElementById("fullName");
-const address = document.getElementById("address");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
 function checkForm(){
-    if (fullName.value == ""){
+    const fullName = document.getElementById("fullName");
+    const address = document.getElementById("address");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    if (fullName.value === ""){
         fullName.style.backgroundColor = "red";
     }
     else{
         fullName.style.backgroundColor = "white";
     }
-    if (address.value == ""){
+    if (address.value === ""){
         address.style.backgroundColor = "red";
     }
     else{
         address.style.backgroundColor = "white";
     }
-    if (email.value == ""){
+    if (email.value === ""){
         email.style.backgroundColor = "red";
     }
     else{
         email.style.backgroundColor = "white";
     }
-    if (phone.value == ""){
+    if (phone.value === ""){
         phone.style.backgroundColor = "red";
     }
     else{
